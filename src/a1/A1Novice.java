55d15ffe0d7b408990187159ab24a4f72744124a
cdotglazewski <cdotglazewski@scjaross-iphone.wireless-1x.unc.edu>
2019-08-31 13:40:12 -0400

    Fixed for loops to read entries and created an array for the output

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,32 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		int times = scan.nextInt();
+		String[] output = new String[times];
+		
+		for (int i=0; i<times; i++) {
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int item = scan.nextInt();
+			double totalAll = 0;
+			
+			for (int x=0; x<item; x++) {
+				double numOfItems = scan.nextDouble();
+				String itemName = scan.next();
+				double cost = scan.nextDouble();
+				
+				totalAll += cost * numOfItems;	
+			}
+			
+			
+			output[i] = firstName.charAt(0) + ". " + lastName + String.format("%.2f, totalAll");
+			
+		}
+		
+		for (int j=0; j<times; j++) {
+			System.out.println(output[j]);
+		}
 		
 	}
+	
 }
