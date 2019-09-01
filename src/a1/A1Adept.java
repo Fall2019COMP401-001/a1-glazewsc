ebe3a7eb43ca8390599585dc5ea6825a6aaedcc5
cdotglazewski <cdotglazewski@scjaross-iphone.wireless-1x.unc.edu>
2019-09-01 17:02:31 -0400

    added for loops to compile arrays and compare max/mins - test w/o print

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -21,8 +21,57 @@ public class A1Adept {
 		double[] totalPrice = new double[numCustomers];
 		String[] customer = new String[numCustomers];
 		
+		double itemPrice;
+		int itemsBought;
+		String product;
 		
+		for (int i=0; i<numCustomers; i++) {
+			double tempTotal = 0;
+			customer[i] = scan.next() + " " + scan.next();
+			itemsBought = scan.nextInt();
+			
+			for (int x=0; x<itemsBought; x++) {
+				itemPrice = scan.nextDouble();
+				product = scan.next();
+				
+				for (int j=0; j<numOfItems; j++) {
+					if (product.equals(item[j])) {
+						tempTotal += (itemPrice * itemCost[j]);
+					}
+				}
+			}
+			
+			totalPrice[i] = tempTotal;
+			
+		}
 		
+		double smallestTotal = totalPrice[0];
+		double biggestTotal = totalPrice[0];
+		String smallestCustomer = customer[0];
+		String biggestCustomer = customer[0];
+		double allTotals = 0;
+		
+		for (int i=0; i<totalPrice.length; i++) {
+			if (totalPrice[i] > biggestTotal) {
+				biggestCustomer = customer[i];
+				biggestTotal = totalPrice[i];
+			}
+		}
+		
+		for (int i=0; i<totalPrice.length; i++) {
+			if (totalPrice[i] < smallestTotal) {
+				smallestCustomer = customer[i];
+				smallestTotal = totalPrice[i];
+			}
+		}
+		
+		for (int i=0; i<totalPrice.length; i++) {
+			allTotals += totalPrice[i];
+		}
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
 		
 	}
 }
